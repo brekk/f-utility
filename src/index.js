b8abc5c34072f9fe5e8ed6a10a6ba71aeed0243a
Brekk <brekk@brekkbockrath.com>
2017-07-16 20:55:57 -0700

    fix published functions (#25)

--- src/index.js
+++ src/index.js
@@ -1,23 +1,35 @@
 export * from 'katsu-curry'
-export * from './array'
-export * from './choice'
-export * from './filter'
-export * from './flip'
-export * from './future'
-export * from './iterate'
-export * from './map'
-export * from './math'
+export {join} from './array'
+export {choice} from './choice'
+export {filter} from './filter'
+export {flip} from './flip'
+export {fork} from './future'
+export {iterate} from './iterate'
+export {map} from './map'
+export {add, subtract, divide, multiply, pow} from './math'
+export {reduce} from './reduce'
+export {reject} from './reject'
+export {split, trim} from './string'
+export {ternary} from './ternary'
+export {triplet} from './triplet'
+export {
+  isTypeof,
+  isBoolean,
+  isNumber,
+  isFunction,
+  isObject,
+  isNil
+} from './types'
+export {
+  which,
+  some,
+  every
+} from './which'
+
 import {random as _random} from './random'
 import * as f from './random-floor'
 import * as t from './random-take'
 import * as w from './random-word'
 import * as s from './random-shuffle'
-export * from './reduce'
-export * from './reject'
-export * from './string'
-export * from './ternary'
-export * from './triplet'
-export * from './types'
-export * from './which'
 
 export const random = Object.assign(_random, f, t, w, s)
