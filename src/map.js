b05d54147a06d98402e1f7ed3468f4dbeb63d57f
Brekk <brekk@brekkbockrath.com>
2017-07-19 18:32:53 -0700

    map delegates correctly now (#26)

--- src/map.js
+++ src/map.js
@@ -1,12 +1,27 @@
 import fastMap from 'fast.js/map'
-import {flip} from './flip'
+import {curry} from 'katsu-curry'
+import FL from 'fantasy-land'
+import {e1} from 'entrust'
+
+const has = curry((x, y) => !!y[x])
+const hasMap = has(`map`)
+const hasFLMap = has(FL.map)
+const _map = e1(`map`)
+const flMap = e1(FL.map)
 
 /**
- * iterable.map(fn) but curried and fast
+ * functor.map(fn) but curried and fast (though will delegate to the functor)
  * @method map
- * @param {function} transform
- * @param {Array} iterable
+ * @param {function} fn
+ * @param {Array} functor
  * @returns {Array} mapped iterable
  * @public
  */
-export const map = flip(fastMap)
+export const map = curry(
+  (fn, functor) => {
+    // if it's an array we wanna pass to fastMap
+    if (hasMap(functor) && !Array.isArray(functor)) return _map(fn, functor)
+    if (hasFLMap(functor)) return flMap(fn, functor)
+    return fastMap(functor, fn)
+  }
+)
---
+++ src/map.spec.js
@@ -0,0 +1,61 @@
+import test from 'ava'
+import {pipe, I} from 'katsu-curry'
+import mapR from 'ramda/src/map'
+import {Left} from 'fantasy-eithers'
+import FL from 'fantasy-land'
+import {map} from './map'
+
+test(`map`, (t) => {
+  t.is(typeof map, `function`)
+  t.is(typeof map(I), `function`)
+  const double = (x) => x * 2
+  const partialIdentity = map(I)
+  const partialDouble = map(double)
+  const abcO = {a: 1, b: 2, c: 3}
+  const three = [1, 2, 3]
+  const abc = `abc`.split(``)
+  t.deepEqual(map(I, abc), abc)
+  t.deepEqual(partialIdentity(abc), abc)
+  t.deepEqual(map(I, three), three)
+  t.deepEqual(map(double, three), [2, 4, 6])
+  t.deepEqual(partialDouble(three), [2, 4, 6])
+  t.deepEqual(map(I, abcO), abcO)
+  t.deepEqual(partialIdentity(abcO), abcO)
+  t.deepEqual(map(double, abcO), {a: 2, b: 4, c: 6})
+  t.deepEqual(partialDouble(abcO), {a: 2, b: 4, c: 6})
+  t.deepEqual(partialDouble(abcO, {}), {a: 2, b: 4, c: 6})
+})
+
+const doubleX = pipe(
+  ({x}) => ({x: x * 2})
+)
+const ourDoubler = map(doubleX)
+const theirDoubler = mapR(doubleX)
+
+test(`map should work like ramda's map`, (t) => {
+  const toUpper = (x) => x.toUpperCase()
+  const ours = map(toUpper)
+  const theirs = map(toUpper)
+  const input = `123aBcDeF`
+  t.deepEqual(ours(input), theirs(input))
+  const input2 = `123aBcDeF`.split(``)
+  t.deepEqual(ours(input2), theirs(input2))
+  const immutable = Left({x: 100})
+  t.deepEqual(theirDoubler(immutable), immutable)
+  t.deepEqual(ourDoubler(immutable), immutable)
+})
+test(`map should delegate to fantasy-land map, if present`, (t) => {
+  function MyFunctor(x) {
+    if (!(this instanceof MyFunctor)) {
+      return new MyFunctor(x)
+    }
+    this.value = x
+    return this
+  }
+  MyFunctor.prototype[FL.map] = function fantasyMap(fn) {
+    return MyFunctor(fn(this.value))
+  }
+  const custom = MyFunctor({x: 100})
+  t.is(theirDoubler(custom).value.x, custom.value.x * 2)
+  t.is(ourDoubler(custom).value.x, custom.value.x * 2)
+})
