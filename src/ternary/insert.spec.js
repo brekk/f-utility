917ee4d4fbc80b45bc2c78801176467c220ed42e
brekk <brekk@brekkbockrath.com>
2020-06-22 21:17:23 +0200

    update insert cases and update package.json

--- src/ternary/insert.spec.js
+++ src/ternary/insert.spec.js
@@ -5,10 +5,17 @@ exam("insert", F => () => {
   expect(F.insert(2, "xyz".split(""), [1, 2, 3, 4])).toEqual([
     1,
     2,
-    ["x",
-    "y",
-    "z"],
+    ["x", "y", "z"],
     3,
     4
   ])
+
+  expect(
+    F.pipe(
+      F.range(0),
+      F.insert(100, 100), // errors?
+      F.box,
+      F.ap([F.length, F.mean, F.median, F.mode])
+    )(100)
+  ).toEqual([102, 50.490196078431374, 51, 100])
 })
